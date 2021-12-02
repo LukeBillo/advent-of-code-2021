09b4fb66f09af29a3a0842f4d0ff2e4aa89d45cf
LukeBillo <luke.o.billington@gmail.com>
2021-12-02 18:30:37 +0000

    Added day 1 + sln for advent of code 2021

---
+++ Day1/part-1.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+const string submarineDepthsReport = "submarine-depths.txt";
+
+var depths = File.ReadAllLines(submarineDepthsReport)
+    .Select(int.Parse)
+    .ToList();
+
+var depthIncreases = 0;
+var previousDepth = int.MaxValue;
+
+foreach (var depth in depths)
+{
+    if (depth > previousDepth)
+    {
+        depthIncreases += 1;
+    }
+
+    previousDepth = depth;
+}
+
+Console.WriteLine($"Depth increases: {depthIncreases}, number of recorded depths: {depths.Count}");
+*/
---
+++ Day1/part-2.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+const string submarineDepthsReport = "submarine-depths.txt";
+
+var depths = File.ReadAllLines(submarineDepthsReport)
+    .Select(int.Parse)
+    .ToList();
+
+var depthIncreases = 0;
+var previousDepthSum = int.MaxValue;
+var threeMeasurements = new List<int>();
+
+for (var i = 0; i < depths.Count; i++)
+{
+    var depth = depths[i];
+    
+    if (threeMeasurements.Count < 3)
+    {
+        threeMeasurements.Add(depth);
+    }
+    else
+    {
+        threeMeasurements[i % 3] = depth;
+    }
+
+    if (threeMeasurements.Count != 3)
+    {
+        continue;
+    }
+
+    var sumOfDepths = threeMeasurements.Sum();
+    if (sumOfDepths > previousDepthSum)
+    {
+        depthIncreases += 1;
+    }
+    
+    previousDepthSum = sumOfDepths;
+}
+
+Console.WriteLine($"Depth increases: {depthIncreases}, number of recorded depths: {depths.Count}");
