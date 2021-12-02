fba91778084dcbca7c39e513d90ccfee1ab30887
LukeBillo <luke.o.billington@gmail.com>
2021-12-02 18:34:12 +0000

    Day 1 part 2 clean-up

--- Day1/part-2.cs
+++ Day1/part-2.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-const string submarineDepthsReport = "submarine-depths.txt";
+﻿const string submarineDepthsReport = "submarine-depths.txt";
 
 var depths = File.ReadAllLines(submarineDepthsReport)
     .Select(int.Parse)
@@ -8,27 +6,10 @@ var depths = File.ReadAllLines(submarineDepthsReport)
 
 var depthIncreases = 0;
 var previousDepthSum = int.MaxValue;
-var threeMeasurements = new List<int>();
 
 for (var i = 0; i < depths.Count; i++)
 {
-    var depth = depths[i];
-    
-    if (threeMeasurements.Count < 3)
-    {
-        threeMeasurements.Add(depth);
-    }
-    else
-    {
-        threeMeasurements[i % 3] = depth;
-    }
-
-    if (threeMeasurements.Count != 3)
-    {
-        continue;
-    }
-
-    var sumOfDepths = threeMeasurements.Sum();
+    var sumOfDepths = depths.Skip(i).Take(3).Sum();
     if (sumOfDepths > previousDepthSum)
     {
         depthIncreases += 1;
